b4e1c546d087065a6ec019093108cc0d66d6d53f
shin <arsenepiskoposov@gmail.com>
2026-04-19 19:04:52 +0500

    апытаюсь понять импорты

--- car.py
+++ car.py
@@ -1,14 +1,15 @@
+import main
 class car():
     def __init__(self, speed, fuel):
         self.speed = speed
         self.fuel = fuel
 
     def acceleration(self):
-        time_to_100 = (100*(10/36) - self.speed*(10/36))/5  # в среднем ускорение авто
+        time_to_100 = (100*(main.KMH_TO_MS) - self.speed*(main.KMH_TO_MS))/main.ACCELERATION_MS2  # в среднем ускорение авто
         return f'Разгон до 100 за {time_to_100:.0f} секунд'
 
     def fuel_value(self) -> float:
-        fuel_balance=self.fuel/6
+        fuel_balance=self.fuel/main.FUEL_CONSUMPTION_L_PER_100KM
         distance = fuel_balance*100
         return f'запас хода: {distance:.0f} километров'
 
@@ -16,3 +17,4 @@ class car():
 
 lexus=car(50, 20)
 print(lexus.acceleration(),'|', lexus.fuel_value())
+print(main.car_discription('Lexus Infinity', 'sky-blue'))
--- main.py
+++ main.py
@@ -2,3 +2,5 @@
 KMH_TO_MS = 10 / 36      # перевод км/ч в м/с
 ACCELERATION_MS2 = 5.0   # среднее ускорение, м/с²
 FUEL_CONSUMPTION_L_PER_100KM = 6.0  # л/100 км
+def car_discription(car_model, color):
+    return f"Описание авто: {car_model}, цвет: {color}"
