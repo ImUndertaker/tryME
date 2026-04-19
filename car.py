a04627ee2434547587c45252f85d6940797bc744
shin <arsenepiskoposov@gmail.com>
2026-04-19 14:04:17 +0500

    f

--- car.py
+++ car.py
@@ -1,20 +1,16 @@
-import KMH_TO_MS, ACCELERATION_MS2, FUEL_CONSUMPTION_L_PER_100KM
-
 class car():
     def __init__(self, speed, fuel):
         self.speed = speed
         self.fuel = fuel
 
     def acceleration(self):
-        time_to_100 = (100*(KMH_TO_MS) - self.speed*(KMH_TO_MS))/ACCELERATION_MS2 # в среднем ускорение авто
+        time_to_100 = (100*(10/36) - self.speed*(10/36))/5  # в среднем ускорение авто
         return f'Разгон до 100 за {time_to_100:.0f} секунд'
 
     def fuel_value(self) -> float:
-        fuel_balance=self.fuel/FUEL_CONSUMPTION_L_PER_100KM
+        fuel_balance=self.fuel/6
         distance = fuel_balance*100
         return f'запас хода: {distance:.0f} километров'
-
-
-
+        
 lexus=car(50, 20)
 print(lexus.acceleration(),'|', lexus.fuel_value())
